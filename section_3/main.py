3bcddb6198f30f7f66aa0a586fc8c386f4e8d0a3
de Theije, Joost <JdeTheije@deloitte.nl>
2023-11-14 16:07:36 +0100

    adding a car to the db

--- section_3/main.py
+++ section_3/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path, HTTPException, status
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
 from database import cars
@@ -43,3 +43,17 @@ def get_car_by_id(id: int = Path(..., ge=0, lt=1000)):
             detail=f"could not find car with {id=}",
         )
     return car
+
+
+@app.post("/cars", status_code=status.HTTP_201_CREATED)
+def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
+    if len(body_cars) < 1:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, details="no cars to add."
+        )
+    min_id = len(cars.values()) + min_id
+    for car in body_cars:
+        while cars.get(min_id):
+            min_id += 1
+        cars[min_id] = car
+        min_id += 1
